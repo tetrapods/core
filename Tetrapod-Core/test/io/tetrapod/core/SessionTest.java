767f35437c78362c59479fa56e703ad5b48f887f
Scott Fortin <sceutre@gmail.com>
2014-03-17 14:33:19 -0600

    try to hook up travis ci

--- Tetrapod-Core/test/io/tetrapod/core/SessionTest.java
+++ Tetrapod-Core/test/io/tetrapod/core/SessionTest.java
@@ -15,7 +15,7 @@ public class SessionTest {
    public void testClientServer() throws Exception {
       Dispatcher dispatcher = new Dispatcher();
 
-      Server server = new Server(12345, dispatcher);
+      Server server = new Server(12346, dispatcher);
       server.start();
 
       Client client = new Client("localhost", 12345, dispatcher);
@@ -30,11 +30,11 @@ public class SessionTest {
          }
       });
 
-      Util.sleep(500000);
+      Util.sleep(5000);
 
       client.close();
 
-      Util.sleep(500000);
+      Util.sleep(5000);
 
       server.stop();
    }
