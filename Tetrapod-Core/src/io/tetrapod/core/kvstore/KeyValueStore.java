ad7960e61eefe89f1a3bb401d84dd2710cc8d2a2
Scott Fortin <sceutre@gmail.com>
2014-03-24 17:10:57 -0600

    simple to disk key value store

---
+++ Tetrapod-Core/src/io/tetrapod/core/kvstore/KeyValueStore.java
@@ -0,0 +1,150 @@
+package io.tetrapod.core.kvstore;
+
+
+import io.tetrapod.core.rpc.Structure;
+import io.tetrapod.core.serialize.datasources.FileDataSource;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.Callable;
+
+/**
+ * Simple key value store.  Each store has a name which needs to be unique from
+ * all other stores.  Keys are strings and values are Objects.  We use tetrapod serialization
+ * so Objects have to be: Integer, Long, String, Boolean, byte[], or Structure subclasses.
+ * <p>
+ * Note that in general byte[] and Structures are mutable, however they have to be treated as
+ * immutable to work with this class.  If you change an object after adding it to a KeyValueStore
+ * some, all, or none of your change may be persisted to disk. 
+ */
+public class KeyValueStore {
+   
+   public static volatile String DATA_DIRECTORY = "."; 
+
+   private final Map<String,Object> map = new HashMap<>();
+   private final String name;
+   private final String datadir;
+   private final KVEntry entry = new KVEntry(); 
+   
+   private FileDataSource out = null;
+   private int fileNumber = 0;
+
+   public KeyValueStore(String name) throws IOException {
+      this(name, DATA_DIRECTORY);
+   }
+   
+   public KeyValueStore(String name, String datadir) throws IOException {
+      this.name = name;
+      this.datadir = datadir;
+      load();
+   }
+   
+   public synchronized int size() { 
+      return map.size(); 
+   }
+   
+   public synchronized Object get(String key) {
+      return map.get(key);
+   }
+   
+   public synchronized void close() throws IOException {
+      out.close();
+   }
+   
+   public synchronized void put(String key, Object value) throws IOException {
+      persistPut(key, value);
+      map.put(key, value);
+   }
+
+   public synchronized void remove(String key) throws IOException {
+      persistRemove(key);
+      map.remove(key);
+   }
+
+   public synchronized void clear() throws IOException {
+      persistClear();
+      map.clear();
+   }
+   
+   public synchronized void register(int contractId, int structId, Callable<Structure> factory) {
+      entry.factory.add(contractId, structId, factory);
+   }
+
+   private void load() throws IOException {
+      File dir = new File(datadir);
+      String[] files = dir.list(new FilenameFilter() {
+         @Override
+         public boolean accept(File dir, String n) {
+            return n.startsWith(name);
+         }
+      });
+      // find the biggest even number, then read it as a checkpoint and any odd numbers above it
+      int biggestEven = 0;
+      int biggest = 0;
+      for (String f : files) {
+         int n = Integer.parseInt(f.substring(name.length() + 1));
+         if (n % 2 == 0 && n > biggestEven)
+            biggestEven = n;
+         if (n > biggest)
+            biggest = n;
+      }
+      if (biggestEven > 0)
+         load(biggestEven);
+      for (int i = biggestEven + 1; i <= biggest; i += 2) 
+         load(i);
+      
+      fileNumber = (fileNumber % 2 == 1) ? fileNumber + 2 : fileNumber + 1;
+      Path path = FileSystems.getDefault().getPath(datadir, name + "." + fileNumber);
+      out = FileDataSource.forAppending(path);
+   }
+   
+   private void load(int i) throws IOException {
+      Path path = FileSystems.getDefault().getPath(datadir, name + "." + i);
+      if (Files.exists(path)) {
+         fileNumber = i;
+         FileDataSource f = FileDataSource.forReading(path);
+         while (!f.atEnd()) {
+            entry.read(f);
+            switch (entry.command) {
+               case KVEntry.COMMAND_CLEAR:
+                  map.clear();
+                  break;
+               case KVEntry.COMMAND_REMOVE:
+                  map.remove(entry.key);
+                  break;
+               case KVEntry.COMMAND_SET:
+                  map.put(entry.key, entry.value);
+                  break;
+            }
+         }
+         f.close();
+      }
+   }
+   
+   private void persistPut(String key, Object value) throws IOException {
+      synchronized (entry) {
+         entry.save(key, value);
+         entry.write(out);
+         out.flush();
+      }
+   }
+
+   private void persistRemove(String key) throws IOException {
+      synchronized (entry) {
+         entry.remove(key);
+         entry.write(out);
+         out.flush();
+      }
+   }
+   
+   private void persistClear() throws IOException {
+      synchronized (entry) {
+         entry.clear();
+         entry.write(out);
+         out.flush();
+      }
+   }
+   
+
+}
