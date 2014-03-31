db13f2aa7912f38c1de78e2b068d72877a4e0666
J Spencer <jspencer@ualberta.net>
2014-03-31 11:27:13 -0700

    Broadcast test for RemoteTestService

--- Tetrapod-Core/src/io/tetrapod/clienttesting/RemoteTestService.java
+++ Tetrapod-Core/src/io/tetrapod/clienttesting/RemoteTestService.java
@@ -3,46 +3,54 @@ package io.tetrapod.clienttesting;
 import java.util.concurrent.TimeUnit;
 
 import io.tetrapod.core.DefaultService;
-import io.tetrapod.core.registry.Topic;
 import io.tetrapod.core.rpc.*;
 import io.tetrapod.core.rpc.Error;
 import io.tetrapod.protocol.clienttesting.*;
+import io.tetrapod.protocol.core.*;
 
 import org.slf4j.*;
 
 public class RemoteTestService extends DefaultService implements RemoteTestContract.API {
    private static final Logger logger = LoggerFactory.getLogger(RemoteTestService.class);
    private DatatypeTestMessage testMessage;
-   private Topic testTopic;
-   
+   private int                 testTopicId;
+
    public RemoteTestService() {
       setMainContract(new RemoteTestContract());
       testMessage = new DatatypeTestMessage();
       testMessage.unicodeNormalization = "e\u0301 should look like \u00E9";
       testMessage.unicodeOutsideBMP = "Earth:\u0001F30D Sun:\u0001F31E";
    }
-   
-   
+
    @Override
    public void onRegistered() {
-      testTopic = null; //not sure how to get a new topic from the registry
-      scheduleRepeatedBroadcast();
-
+      sendRequest(new PublishRequest(), getParentId()).handle(new ResponseHandler() {
+         @Override
+         public void onResponse(Response res) {
+            if (res.isError()) {
+               fail("Can't publish topic", res.errorCode());
+            } else {
+               PublishResponse r = (PublishResponse) res;
+               testTopicId = r.topicId;
+               scheduleRepeatedBroadcast();
+            }
+         }
+      });
    }
-   
+
    @Override
    public void onShutdown(boolean restarting) {
       // TODO Auto-generated method stub
-      
+
    }
 
    private void scheduleRepeatedBroadcast() {
-      dispatcher.dispatch(2, TimeUnit.SECONDS, new Runnable() {
+      dispatcher.dispatch(3, TimeUnit.SECONDS, new Runnable() {
          public void run() {
             if (dispatcher.isRunning()) {
                try {
-                  logger.info("would be sending broadcast here");
-                  //sendDatatypeTest();
+                  logger.trace("Sending Test Broadcast on {}", testTopicId);
+                  sendDatatypeTest();
                } catch (Throwable e) {
                   logger.error(e.getMessage(), e);
                }
@@ -51,9 +59,9 @@ public class RemoteTestService extends DefaultService implements RemoteTestContr
          }
       });
    }
-   
+
    private void sendDatatypeTest() {
-      sendBroadcastMessage(testMessage, testTopic.topicId);
+      sendBroadcastMessage(testMessage, testTopicId);
    }
 
    @Override
@@ -64,6 +72,5 @@ public class RemoteTestService extends DefaultService implements RemoteTestContr
          return new Error(RemoteTestContract.ERROR_MISSING_GUESS);
       }
    }
-
 
 }
