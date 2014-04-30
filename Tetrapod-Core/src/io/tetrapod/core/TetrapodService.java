653cc6f6d05bca98422ae89c9046611870c26570
Aaron Davidson <phacops@gmail.com>
2014-04-30 14:46:08 -0600

    enable tls by default

--- Tetrapod-Core/src/io/tetrapod/core/TetrapodService.java
+++ Tetrapod-Core/src/io/tetrapod/core/TetrapodService.java
@@ -262,7 +262,7 @@ public class TetrapodService extends DefaultService implements TetrapodContract.
          servers.add(new Server(getWebSocketPort(), new WebSessionFactory("/sockets", true), dispatcher));
 
          // create secure port servers, if configured
-         if (Util.getProperty("tetrapod.tls", false)) {
+         if (Util.getProperty("tetrapod.tls", true)) {
             SSLContext ctx = Util.createSSLContext(new FileInputStream(System.getProperty("tetrapod.jks.file", "cfg/tetrapod.jks")), System
                   .getProperty("tetrapod.jks.pwd", "4pod.dop4").toCharArray());
             servers.add(new Server(getWebSocketSecurePort(), new WebSessionFactory("/sockets", true), dispatcher, ctx, false));
