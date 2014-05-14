e212af664cc6fbbb1d3db0254b63839f5af8aa45
Aaron Davidson <phacops@gmail.com>
2014-05-14 11:57:31 -0600

    Found some more netty buffer leaks

--- Tetrapod-Core/src/io/tetrapod/core/utils/AuthToken.java
+++ Tetrapod-Core/src/io/tetrapod/core/utils/AuthToken.java
@@ -12,10 +12,9 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
- * Helper class used to authenticate messages. Works by taking a message (an array of ints) and
- * computing an HMAC using a shared secret. The token is then a subset(input vals) + HMAC. Upon
- * decoding the subset of input vals are recovered, combined with any values known through other
- * means, and the HMAC is recomputed and checked for validity.
+ * Helper class used to authenticate messages. Works by taking a message (an array of ints) and computing an HMAC using a shared secret. The
+ * token is then a subset(input vals) + HMAC. Upon decoding the subset of input vals are recovered, combined with any values known through
+ * other means, and the HMAC is recomputed and checked for validity.
  * <p>
  * Note that the actual data encoded and decoded in the token is up to the caller.
  */
@@ -25,8 +24,8 @@ public class AuthToken {
    private static Mac        MAC               = null;
 
    /**
-    * Sets the shared secret. Needs to be called before this class is used. Returns false if there
-    * is an error which would typically be Java not having strong crypto available.
+    * Sets the shared secret. Needs to be called before this class is used. Returns false if there is an error which would typically be Java
+    * not having strong crypto available.
     */
    public static boolean setSecret(byte[] secret) {
       try {
@@ -57,18 +56,17 @@ public class AuthToken {
    }
 
    /**
-    * Encodes a auth token. The numInToken parameter is a bandwidth micro-optimization. Say for
-    * example we wanted to make an auth token with one value being the entityId. We *could* encode
-    * the true value for the entity id in the token, but since it is already present in the header
-    * we could leave it out of the token and supply it at decode time.
+    * Encodes a auth token. The numInToken parameter is a bandwidth micro-optimization. Say for example we wanted to make an auth token with
+    * one value being the entityId. We *could* encode the true value for the entity id in the token, but since it is already present in the
+    * header we could leave it out of the token and supply it at decode time.
     * 
     * @param values the values which form the basis of the token
     * @param numInToken the number of values which will need to be encoded inside the token
     * @return the base64 encoded token
     */
    public static String encode(int[] values, int numInToken) {
+      ByteBuf buf = Unpooled.buffer();
       try {
-         ByteBuf buf = Unpooled.buffer();
          ByteBufDataSource bds = new ByteBufDataSource(buf);
          for (int i = 0; i < values.length; i++) {
             bds.writeVarInt(values[i]);
@@ -83,37 +81,43 @@ public class AuthToken {
             bds.writeVarInt(values[i]);
          }
          String plainText = Base64.encode(buf, Base64Dialect.URL_SAFE).toString(Charset.forName("UTF-8"));
-         String macText = Base64.encode(Unpooled.wrappedBuffer(mac), Base64Dialect.URL_SAFE).toString(Charset.forName("UTF-8"));
-         return plainText + macText;
+         ByteBuf macBuf = Unpooled.wrappedBuffer(mac);
+         try {
+            String macText = Base64.encode(macBuf, Base64Dialect.URL_SAFE).toString(Charset.forName("UTF-8"));
+            return plainText + macText;
+         } finally {
+            macBuf.release();
+         }
       } catch (IOException e) {
 
+      } finally {
+         buf.release();
       }
       return null;
    }
 
    /**
-    * Decodes an auth token. If there is at least one value in the token it assumes the first value
-    * is a timeout value and checks it versus the current time.
+    * Decodes an auth token. If there is at least one value in the token it assumes the first value is a timeout value and checks it versus
+    * the current time.
     * 
-    * @param values the values of the token, the first numInToken elements get filled in from the
-    *           token
+    * @param values the values of the token, the first numInToken elements get filled in from the token
     * @param numInToken the number of values to pull out from the token
     * @param token the base64 encoded token
-    * @param timedOut true if there is at least one value and the first value is less than the
-    *           current time
-    * @return true if it decodes successfully, and as a side effect fills in values with any values
-    *         which were encoded in token
+    * @param timedOut true if there is at least one value and the first value is less than the current time
+    * @return true if it decodes successfully, and as a side effect fills in values with any values which were encoded in token
     */
    public static boolean decode(int[] values, int numInToken, String token) {
+      ByteBuf tokenBuf = Base64.decode(Unpooled.wrappedBuffer(token.getBytes()), Base64Dialect.URL_SAFE);
       try {
-         ByteBuf tokenBuf = Base64.decode(Unpooled.wrappedBuffer(token.getBytes()), Base64Dialect.URL_SAFE);
          ByteBufDataSource bds = new ByteBufDataSource(tokenBuf);
          for (int i = 0; i < numInToken; i++) {
             values[i] = bds.readVarInt();
          }
          String encoded = encode(values, numInToken);
          return encoded.equals(token);
-      } catch (Exception e) {}
+      } catch (Exception e) {} finally {
+         tokenBuf.release();
+      }
       return false;
    }
 
@@ -124,9 +128,9 @@ public class AuthToken {
       long delta = System.currentTimeMillis() - NOT_THAT_LONG_AGO;
       return (int) (delta / 60000);
    }
-   
+
    // encode/decode wrappers for known auth token types
-   
+
    public static String encodeUserToken(int accountId, int entityId, int properties, int timeoutInMinutes) {
       int timeout = AuthToken.timeNowInMinutes() + timeoutInMinutes;
       int[] vals = { timeout, properties, accountId, entityId };
@@ -150,7 +154,7 @@ public class AuthToken {
       int[] vals = { timeout, val1, val2, val3, accountId };
       return AuthToken.encode(vals);
    }
-   
+
    public static Decoded decodeUserToken(String token, int accountId, int entityId) {
       int[] vals = { 0, 0, accountId, entityId };
       if (!decode(vals, 2, token)) {
@@ -162,7 +166,7 @@ public class AuthToken {
       d.timeLeft = vals[0] - timeNowInMinutes();
       return d;
    }
-   
+
    public static Decoded decodeAuthToken1(String token) {
       int[] vals = new int[3];
       if (!decode(vals, 3, token)) {
@@ -174,7 +178,7 @@ public class AuthToken {
       d.timeLeft = vals[0] - timeNowInMinutes();
       return d;
    }
-   
+
    public static Decoded decodeAuthToken2(String token) {
       int[] vals = new int[4];
       if (!decode(vals, 4, token)) {
@@ -200,10 +204,9 @@ public class AuthToken {
    }
 
    public static class Decoded {
-      public int accountId;
-      public int timeLeft;
+      public int   accountId;
+      public int   timeLeft;
       public int[] miscValues;
    }
-
 
 }
