785d32086e6847ab840b6b2d80d848fef59210ef
Aaron Davidson <phacops@gmail.com>
2014-05-14 10:35:20 -0600

    I think this is where the beep went

--- Tetrapod-Core/src/io/tetrapod/core/DefaultService.java
+++ Tetrapod-Core/src/io/tetrapod/core/DefaultService.java
@@ -611,7 +611,7 @@ public class DefaultService implements Service, Fail.FailHandler, CoreContract.A
    }
 
    private static final Set<String> VALID_EXTENSIONS = new HashSet<>(Arrays.asList(new String[] { "html", "htm", "js", "css", "jpg", "png",
-         "gif",                                     }));
+         "gif", "wav"                                     }));
 
    private void recursiveAddWebFiles(String webRootName, File dir, boolean first) throws IOException {
       if (!dir.exists())
