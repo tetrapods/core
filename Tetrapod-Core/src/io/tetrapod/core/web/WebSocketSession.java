3af37ab456dc57824836a8ddfa5c599157dae119
Aaron Davidson <phacops@gmail.com>
2014-05-22 09:56:45 -0600

    Add some debug logging to WebSocketSession to try and determine where the stall is happening

--- Tetrapod-Core/src/io/tetrapod/core/web/WebSocketSession.java
+++ Tetrapod-Core/src/io/tetrapod/core/web/WebSocketSession.java
@@ -5,7 +5,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.websocketx.*;
 import io.netty.util.ReferenceCountUtil;
-import io.tetrapod.core.Session;
+import io.tetrapod.core.*;
 import io.tetrapod.core.json.JSONObject;
 import io.tetrapod.protocol.core.RequestHeader;
 
@@ -14,15 +14,20 @@ import java.util.Map;
 
 import org.slf4j.*;
 
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.Timer.Context;
+
 public class WebSocketSession extends WebHttpSession {
 
-   private static final Logger       logger = LoggerFactory.getLogger(WebSocketSession.class);
+   private static final Logger       logger       = LoggerFactory.getLogger(WebSocketSession.class);
 
    private final String              wsLocation;
 
+   public static final Timer         requestTimes = Metrics.timer(Dispatcher.class, "web", "requests", "time");
+
    private WebSocketServerHandshaker handshaker;
 
-   public WebSocketSession(SocketChannel ch, Session.Helper helper, Map<String,WebRoot> contentRootMap, String wsLocation) {
+   public WebSocketSession(SocketChannel ch, Session.Helper helper, Map<String, WebRoot> contentRootMap, String wsLocation) {
       super(ch, helper, contentRootMap);
       this.wsLocation = wsLocation;
    }
@@ -43,6 +48,8 @@ public class WebSocketSession extends WebHttpSession {
 
    @Override
    protected void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
+      logger.debug("### REQUEST = {} : {}", ctx.channel().remoteAddress(), req.getUri());
+      final Context context = requestTimes.time();
       if (wsLocation.equals(req.getUri())) {
          // Handshake
          WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(wsLocation, null, false);
@@ -55,12 +62,14 @@ public class WebSocketSession extends WebHttpSession {
       } else {
          super.handleHttpRequest(ctx, req);
       }
+      context.stop();
    }
 
    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
       // Check for closing frame
       if (frame instanceof CloseWebSocketFrame) {
-         handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+         //handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+         ctx.channel().close();
          return;
       }
       if (frame instanceof PingWebSocketFrame) {
