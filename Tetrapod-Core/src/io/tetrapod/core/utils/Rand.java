b2476e625ab9a21dfe244382f683512423a19e1a
Scott Fortin <sceutre@gmail.com>
2014-07-03 15:43:57 -0600

    rand util

--- Tetrapod-Core/src/io/tetrapod/core/utils/Rand.java
+++ Tetrapod-Core/src/io/tetrapod/core/utils/Rand.java
@@ -11,4 +11,17 @@ public class Rand {
    public static int nextInt() {
       return r.nextInt();
    }
+
+   public static int nextInt(int max) {
+      return r.nextInt(max);
+   }
+
+   public static String nextBase36String(int numDigits) {
+      StringBuilder sb = new StringBuilder();
+      String digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+      for (int i=0; i<numDigits; i++) {
+         sb.append(digits.charAt(nextInt(36)));
+      }
+      return sb.toString();
+   }
 }
