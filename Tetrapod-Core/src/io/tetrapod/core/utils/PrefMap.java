80654b65510f6f65053884290bba3eb7f80a0fa9
Aaron Davidson <phacops@gmail.com>
2014-07-16 15:44:56 -0600

    Add a simple PrefMap class for basic k/v stuff

---
+++ Tetrapod-Core/src/io/tetrapod/core/utils/PrefMap.java
@@ -0,0 +1,42 @@
+package io.tetrapod.core.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrefMap {
+
+   private final Map<String, String> prefs = new HashMap<>();
+
+   public String getPref(String key, String def) {
+      return prefs.containsKey(key) ? prefs.get(key) : def;
+   }
+
+   public boolean getPref(String key, boolean def) {
+      return prefs.containsKey(key) ? Boolean.parseBoolean(prefs.get(key)) : def;
+   }
+
+   public int getPref(String key, int def) {
+      return prefs.containsKey(key) ? Integer.parseInt(prefs.get(key)) : def;
+   }
+
+   public long getPref(String key, long def) {
+      return prefs.containsKey(key) ? Long.parseLong(prefs.get(key)) : def;
+   }
+
+   public void setPref(String key, String val) {
+      prefs.put(key, val);
+   }
+
+   public void setPref(String key, boolean val) {
+      prefs.put(key, Boolean.toString(val));
+   }
+
+   public void setPref(String key, int val) {
+      prefs.put(key, Integer.toString(val));
+   }
+
+   public void setPref(String key, long val) {
+      prefs.put(key, Long.toString(val));
+   }
+
+}
