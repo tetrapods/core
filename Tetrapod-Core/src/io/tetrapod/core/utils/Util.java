55a13bb2ef09a1513a7065c03d58966dc6e2fa6c
Aaron Davidson <phacops@gmail.com>
2014-08-18 12:12:10 -0600

    Add sme useful constants for milliscond times

--- Tetrapod-Core/src/io/tetrapod/core/utils/Util.java
+++ Tetrapod-Core/src/io/tetrapod/core/utils/Util.java
@@ -16,7 +16,13 @@ import javax.net.ssl.*;
  */
 public class Util {
 
-   public static final SecureRandom random = new SecureRandom();
+   public static final SecureRandom random     = new SecureRandom();
+
+   public static long               ONE_SECOND = 1000;
+   public static long               ONE_MINUTE = ONE_SECOND * 60;
+   public static long               ONE_HOUR   = ONE_MINUTE * 60;
+   public static long               ONE_DAY    = ONE_HOUR * 24;
+   public static long               ONE_WEEK   = ONE_DAY * 7;
 
    /**
     * Sleeps the current thread for a number of milliseconds, ignores interrupts.
