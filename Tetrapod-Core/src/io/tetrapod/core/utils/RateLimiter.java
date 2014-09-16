e727903dfa75b00d381808e447b876b8742abe24
Aaron Davidson <phacops@gmail.com>
2014-09-16 11:50:55 -0600

    Rate limiter

---
+++ Tetrapod-Core/src/io/tetrapod/core/utils/RateLimiter.java
@@ -0,0 +1,55 @@
+package io.tetrapod.core.utils;
+
+/**
+ * A rate gauge that keeps a history of event times
+ */
+public class RateLimiter {
+
+   protected final long[] samples;
+   protected int          cur = -1;
+   protected int          len;
+   protected int          perMillis;
+
+   public RateLimiter(int max, int perMillis) {
+      samples = new long[max];
+   }
+
+   public synchronized void mark() {
+      cur = (cur + 1) % samples.length;
+      samples[cur] = System.currentTimeMillis();
+      len = Math.min(len + 1, samples.length);
+   }
+
+   public synchronized Long getLastValue() {
+      if (len > 0) {
+         return samples[cur];
+      } else {
+         return null;
+      }
+   }
+
+   public synchronized Long getOldestValue() {
+      if (len > 0) {
+         if (len < samples.length) {
+            return samples[0];
+         } else {
+            return samples[(cur + 1) % samples.length];
+         }
+      } else {
+         return null;
+      }
+   }
+
+   public synchronized int getLength() {
+      return len;
+   }
+
+   public synchronized boolean shouldLimit() {
+      if (len >= samples.length) {
+         return (getLastValue() - getOldestValue()) < perMillis;
+      } else {
+         return false;
+      }
+   }
+
+}
