206744e50922f296cc336422c24572eaf0db5476
Aaron Davidson <phacops@gmail.com>
2014-10-06 15:37:32 -0600

    Add handling to @root WebRoute matching so we can send the whole subpath

--- Tetrapod-Core/src/io/tetrapod/core/web/WebRoutes.java
+++ Tetrapod-Core/src/io/tetrapod/core/web/WebRoutes.java
@@ -7,18 +7,29 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class WebRoutes {
 
-   private final Map<String,WebRoute> routes = new ConcurrentHashMap<>();
-   
+   private final Map<String, WebRoute> routes = new ConcurrentHashMap<>();
+
    public void setRoute(String path, int contractId, int structId) {
       // "/api/LoginRequest  contractId=2, structId=1244334
       WebRoute r = new WebRoute(path, structId, contractId);
       routes.put(path, r);
    }
-   
+
    public WebRoute findRoute(String uri) {
       int ix = uri.indexOf('?');
       String path = ix < 0 ? uri : uri.substring(0, ix);
-      WebRoute r = routes.get(path);
+
+
+      WebRoute r = routes.get(path); 
+      if (r == null) {
+         // special handling for @root paths
+         for (WebRoute route : routes.values()) {
+            if (path.startsWith(route.path)) {
+               return route;
+            }
+         }
+      }
+      
       if (r == null) {
          // allow /route/params instead of /route?params
          ix = uri.lastIndexOf('/');
@@ -27,6 +38,5 @@ public class WebRoutes {
       }
       return r;
    }
-   
 
 }
