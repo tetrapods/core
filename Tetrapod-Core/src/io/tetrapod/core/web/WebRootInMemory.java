bf1b97286fa1bc6b5ab6ae9d426277e75ee52683
Barry Kaplan <barry@livespark.com>
2015-04-10 19:01:38 -0700

    a better solution i think

--- Tetrapod-Core/src/io/tetrapod/core/web/WebRootInMemory.java
+++ Tetrapod-Core/src/io/tetrapod/core/web/WebRootInMemory.java
@@ -32,11 +32,6 @@ public class WebRootInMemory implements WebRoot {
       }
       byte[] content = files.get(path);
 
-      // a bit weird but if we don't have the file in memory lets just make extra special sure the user didn't forget a trailing slash
-      if (content == null) {
-         content = files.get(path+"/index.html");
-      }
-
       if (content != null) {
          FileResult r = new FileResult();
          r.contents = content;
@@ -45,6 +40,14 @@ public class WebRootInMemory implements WebRoot {
          r.isIndex = path.endsWith("index.html");
          return r;
       }
+
+      // a bit weird but if we don't have the file in memory lets just make extra special sure the user didn't forget a trailing slash
+      if (files.containsKey(path+"/index.html")) {
+         FileResult r = new FileResult();
+         r.isDirectory = true;
+         return r;
+      }
+
       return null;
    }
    
