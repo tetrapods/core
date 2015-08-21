e4bc7e4d81d471213ebc2dc419bc56eb6b386f06
Aaron Davidson <phacops@gmail.com>
2015-08-20 18:26:37 -0600

    fix warning

--- Tetrapod-Tetrapod/src/io/tetrapod/core/TetrapodService.java
+++ Tetrapod-Tetrapod/src/io/tetrapod/core/TetrapodService.java
@@ -15,8 +15,6 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.codahale.metrics.Counter;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.base64.Base64Dialect;
