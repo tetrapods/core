3ad86930ad3c7426935beac2cacaf734119d5470
Aaron Davidson <phacops@gmail.com>
2015-11-03 14:01:42 -0700

    cherry pick in the new commands so we can read raft-registry commands if we switch core back and forth, even if they are ignored

---
+++ Tetrapod-Tetrapod/src/io/tetrapod/core/storage/DelEntityCommand.java
@@ -0,0 +1,52 @@
+package io.tetrapod.core.storage;
+
+import java.io.*;
+
+import io.tetrapod.raft.Command;
+
+public class DelEntityCommand implements Command<TetrapodStateMachine> {
+
+   public static final int COMMAND_ID = TetrapodStateMachine.DEL_ENTITY_COMMAND_ID;
+
+   private int entityId;
+
+   public DelEntityCommand() {}
+
+   public DelEntityCommand(int entityId) {
+      this.entityId = entityId;
+   }
+
+   @Override
+   public void applyTo(TetrapodStateMachine state) {
+      state.delEntity(entityId);
+   }
+
+   @Override
+   public void write(DataOutputStream out) throws IOException {
+      out.writeInt(entityId);
+   }
+
+   @Override
+   public void read(DataInputStream in, int fileVersion) throws IOException {
+      entityId = in.readInt();
+   }
+
+   @Override
+   public int getCommandType() {
+      return COMMAND_ID;
+   }
+
+   @Override
+   public String toString() {
+      return "DelEntityCommand(" + entityId + ")";
+   }
+
+   public static void register(TetrapodStateMachine state) {
+      state.registerCommand(COMMAND_ID, () -> new DelEntityCommand());
+   }
+
+   public int getEntityId() {
+      return entityId;
+   }
+
+}
