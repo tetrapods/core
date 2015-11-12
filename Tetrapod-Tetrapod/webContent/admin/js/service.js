a429ef7df65f4fce4625620592585f3554f21e25
Aaron Davidson <phacops@gmail.com>
2015-11-12 14:10:00 -0700

    fix js reference (i.e. fu js)

--- Tetrapod-Tetrapod/webContent/admin/js/service.js
+++ Tetrapod-Tetrapod/webContent/admin/js/service.js
@@ -42,7 +42,7 @@ define(["knockout", "jquery", "bootbox", "alert", "app", "chart", "modules/build
                   }
                }
                app.server.sendTo("ServiceStatsSubscribe", {}, self.entityId, app.server.logResponse)
-            } else if (result.errorCode == Core.SERVICE_UNAVAILABLE && !s.removed) {
+            } else if (result.errorCode == Core.SERVICE_UNAVAILABLE && !self.removed) {
                setTimeout(function() {
                   subscribe(attempt + 1);
                }, 1000 * attempt);
