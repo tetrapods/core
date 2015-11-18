409baa00b911d35caf808cd3a6005b5eeb6cf085
Aaron Davidson <phacops@gmail.com>
2015-11-18 14:21:39 -0700

    Improve status bit setting/clearing

--- Tetrapod-Core/src/io/tetrapod/core/DefaultService.java
+++ Tetrapod-Core/src/io/tetrapod/core/DefaultService.java
@@ -44,7 +44,7 @@ public class DefaultService
    protected final LogBuffer               logBuffer;
    protected SSLContext                    sslContext;
 
-   private ServiceConnector                serviceConnector;
+   protected ServiceConnector              serviceConnector;
 
    protected final ServiceStats            stats;
    protected boolean                       startPaused;
@@ -182,7 +182,7 @@ public class DefaultService
             if (dependenciesReady()) {
                try {
                   if (startPaused) {
-                     updateStatus(getStatus() | Core.STATUS_PAUSED);
+                     setStatus(Core.STATUS_PAUSED);
                   }
 
                   AdminAuthToken.setSecret(Util.getProperty(AdminAuthToken.SHARED_SECRET_KEY));
@@ -196,7 +196,7 @@ public class DefaultService
                   fail(t);
                }
                // ok, we're good to go
-               updateStatus(getStatus() & ~Core.STATUS_STARTING);
+               clearStatus(Core.STATUS_STARTING);
                onStarted();
                if (startPaused) {
                   onPaused();
@@ -214,22 +214,23 @@ public class DefaultService
     */
    private void checkHealth() {
       if (!isShuttingDown()) {
-         if (dispatcher.workQueueSize.getCount() > 0) {
-            logger.warn("DISPATCHER QUEUE SIZE = {} ({} threads busy)", dispatcher.workQueueSize.getCount(), dispatcher.getActiveThreads());
-         }
+         try {
+            if (dispatcher.workQueueSize.getCount() > 0) {
+               logger.warn("DISPATCHER QUEUE SIZE = {} ({} threads busy)", dispatcher.workQueueSize.getCount(),
+                        dispatcher.getActiveThreads());
+            }
 
-         if (logBuffer.hasErrors()) {
-            updateStatus(getStatus() | Core.STATUS_ERRORS);
-         } else {
-            updateStatus(getStatus() & ~Core.STATUS_ERRORS);
+            int status = 0;
+            if (logBuffer.hasErrors()) {
+               status |= Core.STATUS_ERRORS;
+            }
+            if (logBuffer.hasWarnings()) {
+               status |= Core.STATUS_WARNINGS;
+            }
+            setStatus(status, Core.STATUS_ERRORS | Core.STATUS_WARNINGS);
+         } finally {
+            dispatcher.dispatch(1, TimeUnit.SECONDS, () -> checkHealth());
          }
-         if (logBuffer.hasWarnings()) {
-            updateStatus(getStatus() | Core.STATUS_WARNINGS);
-         } else {
-            updateStatus(getStatus() & ~Core.STATUS_WARNINGS);
-         }
-
-         dispatcher.dispatch(1, TimeUnit.SECONDS, () -> checkHealth());
       }
    }
 
@@ -254,7 +255,7 @@ public class DefaultService
    public void onStarted() {}
 
    public void shutdown(boolean restarting) {
-      updateStatus(getStatus() | Core.STATUS_STOPPING);
+      setStatus(Core.STATUS_STOPPING);
       try {
          onShutdown(restarting);
       } catch (Exception e) {
@@ -438,9 +439,24 @@ public class DefaultService
       terminated = val;
    }
 
-   protected void updateStatus(int status) {
+   /**
+    * Sets these status bits to true
+    */
+   protected void setStatus(int bits) {
+      setStatus(bits, bits);
+   }
+
+   /**
+    * Clear these status bits
+    */
+   protected void clearStatus(int bits) {
+      setStatus(0, bits);
+   }
+
+   protected void setStatus(int bits, int mask) {
       boolean changed = false;
       synchronized (this) {
+         int status = (this.status & ~mask) | bits;
          changed = this.status != status;
          this.status = status;
       }
@@ -452,13 +468,13 @@ public class DefaultService
    @Override
    public void fail(Throwable error) {
       logger.error(error.getMessage(), error);
-      updateStatus(status | Core.STATUS_FAILED);
+      setStatus(Core.STATUS_FAILED);
    }
 
    @Override
    public void fail(String reason) {
       logger.error("FAIL: {}", reason);
-      updateStatus(status | Core.STATUS_FAILED);
+      setStatus(Core.STATUS_FAILED);
    }
 
    /**
@@ -536,9 +552,9 @@ public class DefaultService
                      logger.warn("Service is overloaded. Dispatch time is {}ms", Util.nanosToMillis(dispatchTime - start));
                   }
                   // If it took a while to get dispatched, so set STATUS_OVERLOADED flag as a back-pressure signal
-                  updateStatus(getStatus() | Core.STATUS_OVERLOADED);
+                  setStatus(Core.STATUS_OVERLOADED);
                } else {
-                  updateStatus(getStatus() & ~Core.STATUS_OVERLOADED);
+                  clearStatus(Core.STATUS_OVERLOADED);
                }
 
                final RequestContext ctx = fromSession != null ? new SessionRequestContext(header, fromSession)
@@ -582,7 +598,7 @@ public class DefaultService
          } , Session.DEFAULT_OVERLOAD_THRESHOLD)) {
             // too many items queued, full-force back-pressure
             async.setResponse(new Error(ERROR_SERVICE_OVERLOADED));
-            updateStatus(getStatus() | Core.STATUS_OVERLOADED);
+            setStatus(Core.STATUS_OVERLOADED);
          }
       } else {
          logger.warn("{} No handler found for {}", this, header.dump());
@@ -774,7 +790,7 @@ public class DefaultService
    @Override
    public Response requestPause(PauseRequest r, RequestContext ctx) {
       // TODO: Check admin rights or macaroon
-      updateStatus(getStatus() | Core.STATUS_PAUSED);
+      setStatus(Core.STATUS_PAUSED);
       onPaused();
       return Response.SUCCESS;
    }
@@ -800,7 +816,7 @@ public class DefaultService
    @Override
    public Response requestUnpause(UnpauseRequest r, RequestContext ctx) {
       // TODO: Check admin rights or macaroon
-      updateStatus(getStatus() & ~Core.STATUS_PAUSED);
+      clearStatus(Core.STATUS_PAUSED);
       onUnpaused();
       return Response.SUCCESS;
    }
