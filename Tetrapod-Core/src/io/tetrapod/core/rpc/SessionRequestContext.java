ea6944a1b2d1433a40f7ad92de92bd592002daca
Aaron Davidson <phacops@gmail.com>
2016-08-08 11:28:45 -0700

    validate the accountId in decoded session token

--- Tetrapod-Core/src/io/tetrapod/core/rpc/SessionRequestContext.java
+++ Tetrapod-Core/src/io/tetrapod/core/rpc/SessionRequestContext.java
@@ -66,7 +66,7 @@ public class SessionRequestContext extends RequestContext {
       if (senderSecurity == Security.PUBLIC) {
          // upgrade them to protected if their token is good
          DecodedSession d = LoginAuthToken.decodeSessionToken(authToken, accountId, header.fromParentId);
-         if (d != null && d.timeLeft >= 0) {
+         if (d != null && d.timeLeft >= 0 && d.accountId == accountId) {
             senderSecurity = Security.PROTECTED;
          } else {
             try {
