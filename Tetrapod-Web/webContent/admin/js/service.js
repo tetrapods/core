2dfa92c83716995457c413f4ff3bd5f59d42c24d
J Spencer <jspencer@ualberta.net>
2016-10-06 12:50:49 -0700

    Add a useful title to the service errors dialog. Allow the dalog to be dismissed with escape key.

--- Tetrapod-Web/webContent/admin/js/service.js
+++ Tetrapod-Web/webContent/admin/js/service.js
@@ -212,7 +212,9 @@ define(["knockout", "jquery", "bootbox", "alert", "app", "chart", "modules/build
 
                var text = messages.join("");
                bootbox.dialog({
-                  message: text
+                  title: self.host + ' ' + self.name + ' Service ' + self.entityString() + ' ' + self.build,
+                  message: text,
+                  onEscape: function() {}
                }).find("div.modal-dialog").addClass("service-error-logs-dialog");
             }
          });
